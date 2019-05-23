aa9ab8037d83991a7e6b270ad3d1e3d4a75dcdba
lkm <ljlm0402@naver.com>
2019-05-23 10:24:50 +0900

    frist

---
+++ README.md
@@ -0,0 +1,14 @@
+> Lambda,
+
+람다란, 서버를 프로비저닝하거나 관리하지 않고도 코드를 실행할 수 있게 해주는 컴퓨팅 서비스입니다.
+
+Index
+--------
+
+[1] mysql : mysql connect && response data
+
+Use
+--------
+[1] 해당 패키지가 무거우면 람다에 올릴수 없기 때문에 필요한 모듈만 설치한다.
+[2] package.json - dependencies 원하는 모듈만 넣어두고 npm install
+[3] hander 설정을 파일 명에 맞추고 시작한다.
---
+++ mysql.js
@@ -0,0 +1,89 @@
+'use strict';
+
+// require modules
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+exports.handler = (event, context, callback) => {
+
+
+  /** connection.connect()
+ * : Every method you invoke on a connection is queued and executed in sequence.
+ ** connection 성공 시 확인할 것
+ * : connection.threadId
+ */
+  connection.connect();
+
+  /** connection.query()
+   ** 조인문 사용 시
+   * : var options = {sql: '...', nestTables: true};
+   * : connection.query(options, function (error, results, fields) { ... }
+   */
+  connection.query('SELECT * FROM BANK', function (error, results, fields) {
+    if (error) {
+      console.error('[connection.query]error: ' + error);
+      return;
+    }
+
+    console.log('[connection.query]results', results);
+    console.log('[connection.query]fields', fields);
+    callback(null, results);
+  });
+
+  /** connection.end()
+   * Closing the connection is done using end() which makes sure all remaining queries are executed
+   * before sending a quit packet to the mysql server.
+   */
+  connection.end(function (err) {
+    // The connection is terminated now
+    if (err) {
+      console.error('[connection.end]err: ' + err);
+      connection.destroy()
+      return;
+    }
+    console.log('connection ended');
+  });
+
+  /** connection.destroy();
+   * : immediate Termination of underlying socket. ()= 강제종료)
+   */
+  // connection.destroy();
+
+  /** connection.beginTransaction()
+   * : 트랜젝션관리. commit, rollback 예쩨 포함.
+   */
+  // connection.beginTransaction(function(err) {
+  //      if (err) { throw err; }
+  //      connection.query('INSERT INTO posts SET title=?', title, function (error, results, fields) {
+  //        if (error) {
+  //          return connection.rollback(function() {
+  //            throw error;
+  //          });
+  //        }
+  //
+  //        var log = 'Post ' + results.insertId + ' added';
+  //
+  //        connection.query('INSERT INTO log SET data=?', log, function (error, results, fields) {
+  //          if (error) {
+  //            return connection.rollback(function() {
+  //              throw error;
+  //            });
+  //          }
+  //          connection.commit(function(err) {
+  //            if (err) {
+  //              return connection.rollback(function() {
+  //                throw err;
+  //              });
+  //            }
+  //            console.log('success!');
+  //          });
+  //        });
+  //      });
+  //  });
+};
